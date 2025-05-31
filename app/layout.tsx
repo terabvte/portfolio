773c359bbc16fa08d894ac8fca78aca7a3ceb617
terabvte <marcofediuc@gmail.com>
2025-05-31 10:13:03 +0200

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const InterFont = Inter({
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${InterFont.variable} antialiased `}>{children}</body>
+      <body className={`${InterFont.variable} antialiased `}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
